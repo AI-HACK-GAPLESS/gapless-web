55530cbe7342f4e25f564691ec13481f8ea8cdcc
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-13 08:19:34 +0900

    feat: add create dictionary entries API and mutation hook

---
+++ src/shared/apis/create-dic-entries.ts
@@ -0,0 +1,42 @@
+import toast from 'react-hot-toast';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { PATH_API } from './path';
+
+type Payload = {
+  platform: 'discord' | 'slack';
+  server_id: string;
+  entries: { keyword: string; description: string }[];
+};
+
+export const createDictEntries = async (data: Payload) => {
+  const response = await fetch(PATH_API.dict.create, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to create entries');
+  }
+
+  return response.json();
+};
+
+// query
+export const useCreateEntries = (
+  options?: Omit<UseMutationOptions<Payload, unknown, unknown>, 'mutationKey'>
+) => {
+  return useMutation({
+    mutationFn: createDictEntries,
+    onSuccess: () => {
+      toast.success('successfully create entries.');
+    },
+    onError: () => {
+      toast.error('failed to create entries');
+    },
+    ...options,
+  });
+};
