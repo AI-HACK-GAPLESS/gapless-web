b5cffddd6218758230148f2121282b0b94270275
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-13 08:19:39 +0900

    feat: implement custom server API and mutation hook

---
+++ src/shared/apis/custom-server.ts
@@ -0,0 +1,43 @@
+import toast from 'react-hot-toast';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { PATH_API } from './path';
+
+type Payload = {
+  platform: 'discord' | 'slack';
+  server_id: string;
+  title: string;
+  description: string;
+};
+
+export const customServer = async (data: Payload) => {
+  const response = await fetch(PATH_API.customServer, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    credentials: 'include',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to customizing server');
+  }
+
+  return response.json();
+};
+
+// query
+export const useCustomServer = (
+  options?: Omit<UseMutationOptions<Payload, unknown, unknown>, 'mutationKey'>
+) => {
+  return useMutation({
+    mutationFn: customServer,
+    onSuccess: () => {
+      toast.success('successfully customized the server.');
+    },
+    onError: () => {
+      toast.error('failed to customize the server.');
+    },
+    ...options,
+  });
+};
