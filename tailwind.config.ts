a1573ad601112a035b5c45eeca6aec30809f0310
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-12 13:51:43 +0900

    feat: setting font variables

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+    darkmode: 'class',
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Pretendard', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
