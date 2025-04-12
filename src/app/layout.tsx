8ac42023f5d8e53e3f9e225342d733843767d50c
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-04-13 08:18:45 +0900

    feat: wrap children in ToastProvider and ReactQueryProvider in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import {
+  ReactQueryProvider,
+  ToastProvider,
+} from '@/shared/components/providers';
 
 export const metadata: Metadata = {
   title: 'Gapless',
@@ -44,7 +48,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <ToastProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </ToastProvider>
+      </body>
     </html>
   );
 }
